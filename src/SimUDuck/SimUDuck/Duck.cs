15fd1cf7d00f52384498d787436d69b67f29b69f
Long Mai <long.mai@longility.com>
2011-04-27 23:23:22 -0500

    Added simple duck features

---
+++ src/SimUDuck/SimUDuck/Duck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimUDuck
+{
+    abstract class Duck
+    {
+        public void Quack()
+        {
+            Console.Out.WriteLine("Quack");
+        }
+
+        public void Swim()
+        {
+            Console.Out.WriteLine("Swim");
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ src/SimUDuck/SimUDuck/RedheadDuck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimUDuck
+{
+    class RedheadDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.Out.WriteLine("Redhead Duck");
+        }
+    }
+}
